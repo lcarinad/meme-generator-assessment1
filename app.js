54953c4ae40a562302f248f73b027404e1b95ee8
Lauren DeLago <123128869+lcarinad@users.noreply.github.com>
2023-04-28 13:44:04 -0400

    positioning text
    text is positioned but images now overlay.  need to fix

--- app.js
+++ app.js
@@ -9,31 +9,35 @@ submitBtn.addEventListener("click", function (e) {
   e.preventDefault();
   //create the div
   const newDiv = document.createElement("div");
+
+  const memeImgDiv = document.createElement("div");
   const memeImg = document.createElement("img");
+
   const topMsgDiv = document.createElement("div");
-  const topMsg = document.createElement("div");
+
   const bottomMsgDiv = document.createElement("div");
-  const bottomMsg = document.createElement("div");
+
   const removeBtnDiv = document.createElement("div");
   const removeBtn = document.createElement("button");
+
   memeContainer.append(newDiv);
   memeImg.setAttribute("type", "src");
   memeImg.classList.add("img");
   memeImg.src = imageUrl.value;
-  newDiv.append(memeImg);
+  newDiv.append(memeImgDiv);
+  memeImgDiv.append(memeImg);
   newDiv.append(topMsgDiv);
   newDiv.append(bottomMsgDiv);
-  topMsgDiv.classList.add("msg-div");
-  bottomMsgDiv.classList.add("msg-div");
-  topMsg.classList.add("top-text");
-  bottomMsg.classList.add("bottom-text");
+  memeImgDiv.classList.add("meme-img-div");
+  topMsgDiv.classList.add("top-msg-div");
+  bottomMsgDiv.classList.add("bottom-msg-div");
+
   removeBtnDiv.classList.add("meme-container");
   removeBtn.classList.add("remove-btn");
-  topMsg.innerText = topText.value;
-  bottomMsg.innerText = bottomText.value;
+  topMsgDiv.innerText = topText.value;
+  bottomMsgDiv.innerText = bottomText.value;
   removeBtn.innerText = "Delete your meme";
-  topMsgDiv.append(topMsg);
-  bottomMsgDiv.append(bottomMsg);
+
   removeBtnDiv.append(removeBtn);
   form.reset();
 });
