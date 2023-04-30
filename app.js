168b2c90095f7d6ade967b9cc4a919707379621b
Lauren DeLago <123128869+lcarinad@users.noreply.github.com>
2023-04-29 21:53:41 -0400

    added event listener to remove button

--- app.js
+++ app.js
@@ -4,45 +4,46 @@ const topText = document.querySelector("#optional-top-text-input");
 const bottomText = document.querySelector("#optional-bottom-text-input");
 const memeContainer = document.querySelector("#meme-div");
 
-// function handleClick() {
-form.addEventListener("submit", function (e) {
-  e.preventDefault();
+function handleClick() {
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
 
-  const newDiv = document.createElement("div");
+    const newDiv = document.createElement("div");
 
-  const memeImgDiv = document.createElement("div");
-  const memeImg = document.createElement("img");
+    const memeImgDiv = document.createElement("div");
+    const memeImg = document.createElement("img");
 
-  const topMsgDiv = document.createElement("div");
+    const topMsgDiv = document.createElement("div");
 
-  const bottomMsgDiv = document.createElement("div");
+    const bottomMsgDiv = document.createElement("div");
 
-  const removeBtnDiv = document.createElement("div");
-  const removeBtn = document.createElement("button");
-  memeContainer.append(newDiv);
-  memeImg.setAttribute("type", "src");
-  memeImg.classList.add("img");
-  memeImg.src = imageUrl.value;
-  newDiv.append(memeImgDiv);
-  memeImgDiv.append(memeImg);
-  memeImgDiv.append(topMsgDiv);
-  memeImgDiv.append(bottomMsgDiv);
-  memeImgDiv.classList.add("meme-img-div");
-  topMsgDiv.classList.add("top-msg-div");
-  bottomMsgDiv.classList.add("bottom-msg-div");
+    const removeBtnDiv = document.createElement("div");
+    const removeBtn = document.createElement("button");
+    memeContainer.append(newDiv);
+    memeImg.setAttribute("type", "src");
+    memeImg.classList.add("img");
+    memeImg.src = imageUrl.value;
+    newDiv.append(memeImgDiv);
+    memeImgDiv.append(memeImg);
+    memeImgDiv.append(topMsgDiv);
+    memeImgDiv.append(bottomMsgDiv);
+    memeImgDiv.classList.add("meme-img-div");
+    topMsgDiv.classList.add("top-msg-div");
+    bottomMsgDiv.classList.add("bottom-msg-div");
 
-  removeBtnDiv.classList.add("remove-btn-div");
-  removeBtn.classList.add("remove-btn");
+    removeBtnDiv.classList.add("remove-btn-div");
+    removeBtn.classList.add("remove-btn");
 
-  topMsgDiv.innerText = topText.value;
-  bottomMsgDiv.innerText = bottomText.value;
-  removeBtn.innerText = "Delete your meme";
-  memeImgDiv.append(removeBtnDiv);
-  removeBtnDiv.append(removeBtn);
+    topMsgDiv.innerText = topText.value;
+    bottomMsgDiv.innerText = bottomText.value;
+    removeBtn.innerText = "Delete your meme";
+    memeImgDiv.append(removeBtnDiv);
+    removeBtnDiv.append(removeBtn);
 
-  form.reset();
-});
-// }
-
-// const removeBtn = document.querySelector("button");
-// console.log(event);
+    form.reset();
+    removeBtn.addEventListener("click", function (e) {
+      memeImgDiv.remove();
+    });
+  });
+}
+handleClick();
