dda55f6024f528cd65e5d8ca973908fb6a186449
Lauren DeLago <123128869+lcarinad@users.noreply.github.com>
2023-04-29 21:23:04 -0400

    final div message styling

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const form = document.querySelector("#form");
 let imageUrl = document.querySelector("#image-url");
 const topText = document.querySelector("#optional-top-text-input");
 const bottomText = document.querySelector("#optional-bottom-text-input");
-const submitBtn = document.querySelector("#submit-btn");
 const memeContainer = document.querySelector("#meme-div");
+
 // function handleClick() {
-submitBtn.addEventListener("click", function (e) {
+form.addEventListener("submit", function (e) {
   e.preventDefault();
-  //create the div
+
   const newDiv = document.createElement("div");
 
   const memeImgDiv = document.createElement("div");
@@ -19,27 +19,30 @@ submitBtn.addEventListener("click", function (e) {
 
   const removeBtnDiv = document.createElement("div");
   const removeBtn = document.createElement("button");
-
   memeContainer.append(newDiv);
   memeImg.setAttribute("type", "src");
   memeImg.classList.add("img");
   memeImg.src = imageUrl.value;
   newDiv.append(memeImgDiv);
   memeImgDiv.append(memeImg);
-  newDiv.append(topMsgDiv);
-  newDiv.append(bottomMsgDiv);
+  memeImgDiv.append(topMsgDiv);
+  memeImgDiv.append(bottomMsgDiv);
   memeImgDiv.classList.add("meme-img-div");
   topMsgDiv.classList.add("top-msg-div");
   bottomMsgDiv.classList.add("bottom-msg-div");
 
   removeBtnDiv.classList.add("remove-btn-div");
   removeBtn.classList.add("remove-btn");
+
   topMsgDiv.innerText = topText.value;
   bottomMsgDiv.innerText = bottomText.value;
   removeBtn.innerText = "Delete your meme";
-  newDiv.append(removeBtnDiv);
+  memeImgDiv.append(removeBtnDiv);
   removeBtnDiv.append(removeBtn);
-  console.log("removebtn");
+
   form.reset();
 });
 // }
+
+// const removeBtn = document.querySelector("button");
+// console.log(event);
